3b4b2268c9910727f2cae0000e37a764dd8001c4
MezzUK <bbadcock_uk@yahoo.com>
2021-11-06 09:25:08 +0000

    Final pages and framer motion install with fix

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,19 +4,22 @@ import home1 from '../img/home1.png';
 // Styled Components
 import { About, Description, Image, Hide } from "../styles";
 
+//Framer motion
+import {motion} from 'framer-motion';
+
 const AboutSection = () => {
     return(
         <About>
             <Description>
                 <div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 initial={{opacity: 0}} animate={{opacity: 1, transition:{duration: 2}}}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>you <span>dreams</span> come</h2>
+                        <motion.h2>you <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2>true.</motion.h2>
                     </Hide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. 
