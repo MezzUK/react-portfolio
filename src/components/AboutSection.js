49ec75124b5bfb901279baf0993d152a724a5fd6
MezzUK <bbadcock_uk@yahoo.com>
2021-11-14 10:20:46 +0000

    Numerous animation updates

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,6 +8,8 @@ import { About, Description, Image, Hide } from "../styles";
 import {motion} from 'framer-motion';
 import { titleAnim, fade, photoAnim } from '../animation';
 
+import Wave from '../components/Wave'
+
 const AboutSection = () => {
 
     return(
@@ -33,6 +35,7 @@ const AboutSection = () => {
             <Image>
                 <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
             </Image>
+            <Wave />
         </About>
     );
 };
